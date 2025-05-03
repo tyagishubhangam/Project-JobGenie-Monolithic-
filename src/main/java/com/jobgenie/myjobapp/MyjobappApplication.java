c77709419860064212b339c93275550e2a42467a
Shubhangam Tyagi <tyagishubhangam0090@gmail.com>
2025-05-04 05:05:55 +0530

    render deploy version

--- src/main/java/com/jobgenie/myjobapp/MyjobappApplication.java
+++ src/main/java/com/jobgenie/myjobapp/MyjobappApplication.java
@@ -1,6 +1,6 @@
 package com.jobgenie.myjobapp;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,12 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyjobappApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-
-		// Optional: set as system properties so Spring can access them
-		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
-		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+//		Dotenv dotenv = Dotenv.load();
+//
+//		// Optional: set as system properties so Spring can access them
+//		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+//		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+//		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		System.out.println("Hello World!" + System.getenv("DATABASE_URL"));
 		SpringApplication.run(MyjobappApplication.class, args);
 	}
 
